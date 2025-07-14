213ba7a790740f00504f5673462185e9edb15bc9
mac-max <79422860+mac-max@users.noreply.github.com>
2025-07-14 23:41:55 +0200

    Update fIlter_controller.py

--- fIlter_controller.py
+++ fIlter_controller.py
@@ -1,31 +1,27 @@
-import RPi.GPIO as GPIO
+import pigpio
 import time
 
 class IRFilterController:
     def __init__(self, pin_pulse=2, pin_dir=3, pulse_duration=0.05):
+        self.pi = pigpio.pi()
         self.pin_pulse = pin_pulse
         self.pin_dir = pin_dir
         self.pulse_duration = pulse_duration
-
-        GPIO.setmode(GPIO.BCM)
-        GPIO.setup(self.pin_pulse, GPIO.OUT, initial=GPIO.LOW)
-        GPIO.setup(self.pin_dir, GPIO.OUT, initial=GPIO.LOW)
+        self.pi.set_mode(self.pin_pulse, pigpio.OUTPUT)
+        self.pi.set_mode(self.pin_dir, pigpio.OUTPUT)
 
     def _send_pulse(self):
-        GPIO.output(self.pin_pulse, GPIO.HIGH)
+        self.pi.write(self.pin_pulse, 1)
         time.sleep(self.pulse_duration)
-        GPIO.output(self.pin_pulse, GPIO.LOW)
+        self.pi.write(self.pin_pulse, 0)
 
     def switch_in(self):
-        """IR-Filter einschwenken"""
-        GPIO.output(self.pin_dir, GPIO.HIGH)
+        self.pi.write(self.pin_dir, 1)
         self._send_pulse()
 
     def switch_out(self):
-        """IR-Filter ausschwenken"""
-        GPIO.output(self.pin_dir, GPIO.LOW)
+        self.pi.write(self.pin_dir, 0)
         self._send_pulse()
 
     def cleanup(self):
-        """GPIO-Pins freigeben"""
-        GPIO.cleanup((self.pin_pulse, self.pin_dir))
+        self.pi.stop()
