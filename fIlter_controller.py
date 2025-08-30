8f739e3c5464221c5bf5cd803fdcb6d76ef7c7a5
mac-max <79422860+mac-max@users.noreply.github.com>
2025-08-30 22:43:20 +0200

    Update fIlter_controller.py

--- fIlter_controller.py
+++ fIlter_controller.py
@@ -2,30 +2,33 @@ import pigpio
 import time
 
 class IRFilterController:
-    def __init__(self, pin_pulse=2, pin_dir=3, pulse_duration=0.05):
-        self.pi = pigpio.pi()  # Verbindung zum Daemon
+    def __init__(self, pin_in=2, pin_out=3, pulse_duration=0.05):
+        self.pi = pigpio.pi()
         if not self.pi.connected:
             raise RuntimeError("pigpio daemon is not running")
 
-        self.pin_pulse = pin_pulse
-        self.pin_dir = pin_dir
+        self.pin_in = pin_in      # GPIO for "switch in" pulse
+        self.pin_out = pin_out    # GPIO for "switch out" pulse
         self.pulse_duration = pulse_duration
 
-        self.pi.set_mode(self.pin_pulse, pigpio.OUTPUT)
-        self.pi.set_mode(self.pin_dir, pigpio.OUTPUT)
-
-    def _send_pulse(self):
-        self.pi.write(self.pin_pulse, 1)
-        time.sleep(self.pulse_duration)
-        self.pi.write(self.pin_pulse, 0)
+        self.pi.set_mode(self.pin_in, pigpio.OUTPUT)
+        self.pi.set_mode(self.pin_out, pigpio.OUTPUT)
+        self.pi.write(self.pin_in, 0)
+        self.pi.write(self.pin_out, 0)
 
     def switch_in(self):
-        self.pi.write(self.pin_dir, 1)
-        self._send_pulse()
+        """IR-Filter einschwenken"""
+        self.pi.write(self.pin_out, 0)
+        self.pi.write(self.pin_in, 1)
+        time.sleep(self.pulse_duration)
+        self.pi.write(self.pin_in, 0)
 
     def switch_out(self):
-        self.pi.write(self.pin_dir, 0)
-        self._send_pulse()
+        """IR-Filter ausschwenken"""
+        self.pi.write(self.pin_in, 0)
+        self.pi.write(self.pin_out, 1)
+        time.sleep(self.pulse_duration)
+        self.pi.write(self.pin_out, 0)
 
     def cleanup(self):
         self.pi.stop()
