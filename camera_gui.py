21f8cd031e5b6a45967652dd480e836abaafb699
mac-max <79422860+mac-max@users.noreply.github.com>
2025-10-06 22:48:28 +0200

    Update camera_gui.py

--- camera_gui.py
+++ camera_gui.py
@@ -132,7 +132,7 @@ def open_led_window(root):
         print("[ERROR] LED-Controller konnte nicht geöffnet werden:", e)
 
 
-ttk.Button(root, text="LED Steuerung öffnen", command=open_led_window).pack(pady=10)
+ttk.Button(root, text="LED Steuerung öffnen", command=open_led_window(root)).pack(pady=10)
 ttk.Button(root, text="Kameraeinstellungen", command=lambda: CameraSettings(root, stream)).pack(pady=5)
 ttk.Button(root, text="Spektralsensor anzeigen", command=lambda: SensorMonitor(root)).pack(pady=5)
 
